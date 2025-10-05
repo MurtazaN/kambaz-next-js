d04eb3f2005830562343f4f0ae6382734a01766b
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-05 16:55:19 -0400

    Redesign Kambaz layout and navigation with Bootstrap
    Refactored the Kambaz app layout to use Bootstrap's flexbox utilities instead of tables for improved responsiveness. Updated the navigation to a vertical sidebar with icons and enhanced styling, including a new NEU logo image. Modernized the Dashboard page with Bootstrap Cards and grid layout. Added a global styles.css for consistent styling and layout adjustments.

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
+import { FaAlignJustify } from "react-icons/fa";
+
+
 export default async function CoursesLayout(
   { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
  const { cid } = await params;
-// const { id: cid } = await params;
+
  return (
    <div id="wd-courses">
-     <h2>Courses {cid}</h2>
+     {/* <h2>Courses {cid}</h2>
      <hr />
      <table>
        <tbody>
@@ -15,6 +18,20 @@ export default async function CoursesLayout(
            <td valign="top" width="100%"> {children} </td>
          </tr>
        </tbody>
-     </table>
+     </table> */}
+
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        Course {cid} </h2> <hr />
+        <div className="d-flex">
+          <div className="d-none d-md-block">
+            <CourseNavigation />
+          </div>
+          <div className="flex-fill">
+            {children}
+          </div>
+        </div>
+
    </div>
-);}
+);
+}
--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -1,20 +1,99 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+
+// import { Link } from "react-router-dom";
+
 import Link from "next/link";
+
 export default function KambazNavigation() {
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link href="/Account" id="wd-account-link">Account</Link><br/>
-      <Link href="/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link href="/Dashboard" id="wd-course-link">Courses</Link><br/> {/* Land on the same page as dashboard */}
-      <Link href="/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link href="/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link href="/Labs" id="wd-labs-link">Labs</Link><br/>
-      {/* Confirm if this is needed : calendar, inbox are not implemented yet 
-      
-      <Link href="/Zoom" id="wd-zoom-link">Zoom</Link><br/>
-      <Link href="/Grades" id="wd-grades-link">Grades</Link><br/>*/}
 
-      <Link href="/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link href="/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-    </div>
+  //   <ListGroup className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2" style={{ width: 120 }}
+  //             id="wd-kambaz-navigation">
+  //    <ListGroupItem className="bg-black border-0 text-center" as="a"
+  //             target="_blank" href="https://www.northeastern.edu/" id="wd-neu-link">
+  //       <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
+  //    </ListGroupItem>
+  //    <ListGroupItem className="border-0 bg-black text-center">
+  //      <Link href="/Account" id="wd-account-link" className="text-white text-decoration-none">
+  //        <FaRegCircleUser className="fs-1 text-white" />
+  //        <br />
+  //        Account
+  //      </Link>
+  //    </ListGroupItem>
+  //    <ListGroupItem className="border-0 bg-white text-center">
+  //      <Link href="/Dashboard" id="wd-dashboard-link" className="text-danger text-decoration-none">
+  //        <AiOutlineDashboard className="fs-1 text-danger" />
+  //        <br />
+  //        Dashboard
+  //      </Link>
+  //    </ListGroupItem>
+
+  //    {/* complete styling the rest of the links for  */}
+
+  //     <ListGroupItem className="border-0 bg-black text-center">
+  //       <Link href="/Dashboard" id="wd-courses-link" className="text-white text-decoration-none">
+  //         <LiaBookSolid className="fs-1 text-white" />
+  //         <br />
+  //         Courses
+  //       </Link>
+  //     </ListGroupItem>
+  //     <ListGroupItem className="border-0 bg-black text-center">
+  //       <Link href="/Calendar" id="wd-calendar-link" className="text-white text-decoration-none">
+  //         <IoCalendarOutline className="fs-1 text-white" />
+  //         <br />
+  //         Calendar
+  //       </Link>
+  //     </ListGroupItem>
+  //     <ListGroupItem className="border-0 bg-black text-center">
+  //       <Link href="/Inbox" id="wd-inbox-link" className="text-white text-decoration-none">
+  //         <FaInbox className="fs-1 text-white" />
+  //         <br />
+  //         Inbox
+  //       </Link>
+  //     </ListGroupItem>
+  //     <ListGroupItem className="border-0 bg-black text-center">
+  //       <Link href="/Labs" id="wd-labs-link" className="text-white text-decoration-none">
+  //         <LiaCogSolid className="fs-1 text-white" />
+  //         <br />
+  //         Labs
+  //       </Link>
+  //     </ListGroupItem>
+
+  //  </ListGroup>
+
+
+    <div id="wd-kambaz-navigation" style={{ width: 120 }} className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+            <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank" className="list-group-item bg-black border-0 text-center">
+                <img src="/images/NEU_RGB.png" width="75px" />
+            </a>
+            <Link href="/Account" id="wd-account-link" className="list-group-item text-center border-0 bg-black text-white">
+                <FaRegCircleUser className="fs-1 text text-white" /><br />
+                Account
+            </Link>
+            <Link href="/Dashboard" id="wd-dashboard-link" className="list-group-item text-center border-0 bg-white text-danger">
+                <AiOutlineDashboard className="fs-1 text-danger" /><br />
+                Dashboard
+            </Link>
+            <Link href="/Dashboard" id="wd-course-link" className="list-group-item text-white bg-black text-center border-0">
+                <LiaBookSolid className="fs-1 text-danger" /><br />
+                Courses
+            </Link>
+            <Link href="/Calendar" id="wd-calendar-link" className="list-group-item text-white bg-black text-center border-0">
+                <IoCalendarOutline className="fs-1 text-danger" /><br />
+                Calendar
+            </Link>
+            <Link href="/Inbox" id="wd-inbox-link" className="list-group-item text-white bg-black text-center border-0">
+                <FaInbox className="fs-1 text-danger" /><br />
+                Inbox
+            </Link>
+            <Link href="/Labs" id="wd-labs-link" className="list-group-item text-white bg-black text-center border-0">
+                <LiaCogSolid className="fs-1 text-danger" /><br />
+                Labs
+            </Link>
+        </div>
+    
 );}
