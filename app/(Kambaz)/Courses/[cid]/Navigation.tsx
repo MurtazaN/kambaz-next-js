6d9a5ab40c9fea1cb052e1cd8ad96c18d3ae819b
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-06 14:15:28 -0400

    Remove commented-out legacy navigation code
    Deleted large blocks of commented-out code from both Course and Kambaz navigation components to clean up the codebase and improve readability.

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,33 +1,3 @@
-// import Link from "next/link";
-// export default function CourseNavigation() {
-//   return (
-//     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      
-//       <Link href="/Courses/1234/Home" id="wd-course-home-link"
-//         className="list-group-item active border-0"> Home </Link>
-//       <Link href="/Courses/1234/Modules" id="wd-course-modules-link"
-//         className="list-group-item text-danger border-0"> Modules </Link>
-//       <Link href="/Courses/1234/Piazza" id="wd-course-piazza-link"
-//         className="list-group-item text-danger border-0"> Piazza </Link>
-//       <Link href="/Courses/1234/Zoom" id="wd-course-zoom-link"
-//         className="list-group-item text-danger border-0"> Zoom </Link>
-//       <Link href="/Courses/1234/Assignments" id="wd-course-assignments-link"
-//         className="list-group-item text-danger border-0"> Assignments </Link>
-//       <Link href="/Courses/1234/Quizzes" id="wd-course-quizzes-link"
-//         className="list-group-item text-danger border-0"> Quizzes </Link>
-//       <Link href="/Courses/1234/People/Table" id="wd-course-people-link"
-//         className="list-group-item text-danger border-0" > People </Link>
-      
-//       <Link href="/Courses/1234/Grades" id="wd-course-grades-link"
-//         className="list-group-item text-danger border-0"> Grades </Link>
-//       <Link href="/Courses/1234/Exams" id="wd-course-exams-link"
-//         className="list-group-item text-danger border-0"> Exams </Link>
-//       <Link href="/Courses/1234/Project" id="wd-course-project-link"
-//         className="list-group-item text-danger border-0"> Project </Link>
-//     </div>
-//   );}
-
-
 "use client";
 
 import { usePathname } from "next/navigation";
