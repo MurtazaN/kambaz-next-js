41f689e6941af6f8f1a193506ac36b8567a2f04a
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-05 22:37:31 -0400

    Refactor assignments UI and add assignment controls
    Refactored the assignments and account-related pages to use React Bootstrap components for improved UI consistency. Added new components for assignment controls, assignment control buttons, and green edit icons. Updated the assignments list to a modular, styled layout with grouped sections for assignments, quizzes, projects, and exams. Also updated navigation and authentication pages for better styling and usability. Added 'react-router-dom' as a new dependency.

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -1,24 +1,22 @@
+
+import { Form } from "react-bootstrap";
 import Link from "next/link";
+
+
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
     <h3>Sign up</h3>
-      {/* added default values */}
-      <input defaultValue="John" placeholder="John" className="wd-username" /><br/>
-      <input defaultValue={"jsdhfdsjhb"} placeholder="P@$$Waard" type="password" className="wd-password" /><br/>
-      <input defaultValue={"jsdhfdsjhb"} placeholder="PA$$Waard" type="password" className="wd-password-verify" /><br/>
-
-      {/*<label htmlFor="username">Your User Name:</label> <br/>
-      <input type="text" id="username" name="name" defaultValue="John Doe" /><br/>
-
-      <label htmlFor="password">Password</label> <br/>
-      <input type="password" id="password" name="password" defaultValue="P@$$Waaaard" /><br/>
-
-      <label htmlFor="verify_password">Verify your Password</label> <br/>
-      <input type="password" id="verify_password" name="password" defaultValue="P@$$Waaaard" /><br/> */}
-
-
-      <Link  href="Profile" > Sign up </Link><br />
+      <Form.Control id="wd-username"
+             placeholder="username"
+             className="mb-2"/>
+      <Form.Control id="wd-password"
+             placeholder="password" type="password"
+             className="mb-2"/>
+      <Link id="wd-signin-btn"
+            href="/Account/Profile"
+            className="btn btn-primary w-100 mb-2">
+            Sign up </Link><br />
       <Link  href="Signin" > Sign in </Link>
     </div>
 );}
