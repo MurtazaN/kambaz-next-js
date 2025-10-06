07879f77c7e08550b10861451ec8e72027cca0a3
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-06 18:49:19 -0400

    Add conditional rendering examples to Lab3
    Introduced new components demonstrating conditional rendering: ConditionalOutputIfElse, ConditionalOutputInline, IfElse, and TernaryOperator. Updated Lab3 page to include these components, providing examples of different conditional output techniques in React.

--- app/(Kambaz)/Labs/Lab3/page.tsx
+++ app/(Kambaz)/Labs/Lab3/page.tsx
@@ -1,5 +1,11 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+
 
 export default function Lab3() {
   return (
@@ -7,5 +13,10 @@ export default function Lab3() {
       <h2>Lab 3</h2>
       <VariablesAndConstants />
       <VariableTypes />
+      <BooleanVariables />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <IfElse />
+      <TernaryOperator />
     </div>
 );}
