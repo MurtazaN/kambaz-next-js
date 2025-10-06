e02dd41021b347d70e051bb867b077233cecd0b7
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-06 14:41:42 -0400

    Update BootstrapLists.tsx

--- app/(Kambaz)/Labs/Lab2/BootstrapLists.tsx
+++ app/(Kambaz)/Labs/Lab2/BootstrapLists.tsx
@@ -33,7 +33,7 @@ export default function BootstrapLists() {
                         2001 A Space Odyssey </ListGroup.Item>
                     <ListGroup.Item action disabled
                         href="https://en.wikipedia.org/wiki/Ender%27s_Game">
-                        Ender's Game         </ListGroup.Item>
+                        Enders Game         </ListGroup.Item>
                     <ListGroup.Item action onClick={() => alert("New book added")}>
                         Add another book     </ListGroup.Item>
                 </ListGroup>
