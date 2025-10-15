de70152b57945c4731389f71c4eac84e5765b69e
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-10-14 21:40:46 -0400

    courses and database

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,28 +1,23 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 import { FaAlignJustify } from "react-icons/fa";
+import { courses } from "../../Database";
 
 
 export default async function CoursesLayout(
   { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>) {
- const { cid } = await params;
+  const { cid } = await params;
+  const course = courses.find((course) => course._id === cid);
 
  return (
    <div id="wd-courses">
-     {/* <h2>Courses {cid}</h2>
-     <hr />
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="200"> <CourseNavigation /> </td>
-           <td valign="top" width="100%"> {children} </td>
-         </tr>
-       </tbody>
-     </table> */}
 
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course {cid} </h2> <hr />
+          {course?.name}
+
+        {/* Course {cid}  */}
+        </h2>
         <div className="d-flex">
           <div className="d-none d-md-block">
             <CourseNavigation />
@@ -33,5 +28,5 @@ export default async function CoursesLayout(
         </div>
 
    </div>
-);
+  );
 }
