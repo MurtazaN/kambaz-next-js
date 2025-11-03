6d4930f9b2b874f9338f8ef54525b2f48decb683
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-11-03 18:23:56 -0500

    Add ModuleEditor component and integrate in ModulesControls
    Introduced a new ModuleEditor modal component for adding modules. Updated ModulesControls to use ModuleEditor, managing its visibility with local state and passing necessary props for module creation.

--- app/(Kambaz)/Courses/[cid]/Modules/ModulesControls.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/ModulesControls.tsx
@@ -1,4 +1,5 @@
-
+import ModuleEditor from "./ModuleEditor";
+import { useState } from "react";
 
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from "react-bootstrap";
 import { FaPlus } from "react-icons/fa6";
@@ -6,7 +7,13 @@ import { MdDoNotDisturbAlt } from "react-icons/md";
 import GreenCheckmark from "./GreenCheckmark";
 
 
-export default function ModulesControls() {
+export default function ModulesControls(
+    { moduleName, setModuleName, addModule }:
+        { moduleName: string; setModuleName: (title: string) => void; addModule: () => void; }) {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div id="wd-modules-controls" className="text-nowrap">
             <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-module-btn">
@@ -45,6 +52,10 @@ export default function ModulesControls() {
             <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-collapse-all">
                 Collapse All
             </Button>
+
+            <ModuleEditor show={show} handleClose={handleClose} dialogTitle="Add Module"
+                moduleName={moduleName} setModuleName={setModuleName} addModule={addModule} />
+
         </div>
     );
 }
