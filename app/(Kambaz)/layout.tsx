05e1f17b7311d71e130877a901c73be1407670c8
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-11-20 11:29:13 -0500

    Add session management to Kambaz layout
    Introduced a new Session component to handle user session fetching and state management. Updated the layout to wrap content with Session, ensuring user profile is loaded before rendering children. Also updated the signout function in Profile to await client signout.

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -7,20 +7,23 @@ import "./styles.css";
 import store from "./store";
 import { Provider } from "react-redux";
 
+import Session from "./Account/Session";
 
 export default function KambazLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <Provider store={store}>
-      <div id="wd-kambaz">
-        <div className="d-flex">
-          <div>
-            <KambazNavigation />
-          </div>
-          \    <div className="wd-main-content-offset p-3 flex-fill">
-            {children}
+      <Session>
+        <div id="wd-kambaz">
+          <div className="d-flex">
+            <div>
+              <KambazNavigation />
+            </div>
+            \    <div className="wd-main-content-offset p-3 flex-fill">
+              {children}
+            </div>
           </div>
         </div>
-      </div>
+      </Session>
     </Provider>
 
   );
