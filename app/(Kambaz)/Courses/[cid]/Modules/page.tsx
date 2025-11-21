86ded24d14b7b4b8f47a8e8f15a58926e91c9b36
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-11-21 18:31:50 -0500

    Update page.tsx

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -30,16 +30,16 @@ export default function Modules() {
     const onCreateModuleForCourse = async () => {
         if (!cid) return;
         const newModule = { name: moduleName, course: cid };
-        const module = await client.createModuleForCourse((cid as string), newModule);
-        dispatch(setModules([...modules, module]));
+        const createdModule = await client.createModuleForCourse((cid as string), newModule);
+        dispatch(setModules([...modules, createdModule]));
     };
     const onRemoveModule = async (moduleId: string) => {
         await client.deleteModule(moduleId);
         dispatch(setModules(modules.filter((m: any) => m._id !== moduleId)));
     };
-    const onUpdateModule = async (module: any) => {
-        await client.updateModule(module);
-        const newModules = modules.map((m: any) => m._id === module._id ? module : m);
+    const onUpdateModule = async (currentModule: any) => {
+        await client.updateModule(currentModule);
+        const newModules = modules.map((m: any) => m._id === currentModule._id ? currentModule : m);
         dispatch(setModules(newModules));
     };
     return (
@@ -52,36 +52,37 @@ export default function Modules() {
             <br />
             <ListGroup id="wd-modules" className="rounded-0">
                 {modules
-                    .map((module: any) => (
+                    .map((currentModule: any) => (
                         <ListGroupItem
-                            key={module._id}
+                            key={currentModule._id}
                             className="wd-module p-0 mb-5 fs-5 border-gray"
                         >
                             <div className="wd-title p-3 ps-2 bg-secondary">
                                 <BsGripVertical className="me-2 fs-3" />
-                                {!module.editing && module.name}
-                                {module.editing && (
+                                {!currentModule.editing && currentModule.name}
+                                {currentModule.editing && (
                                     <FormControl
                                         className="w-50 d-inline-block"
                                         onChange={(e) =>
-                                            dispatch(updateModule({ ...module, name: e.target.value }))
+                                            dispatch(updateModule({ ...currentModule, name: e.target.value }))
                                         }
                                         onKeyDown={(e) => {
                                             if (e.key === "Enter") {
-                                                onUpdateModule({ ...module, editing: false });
+                                                onUpdateModule({ ...currentModule, editing: false });
                                             }
                                         }}
-                                        defaultValue={module.name}
+                                        defaultValue={currentModule.name}
                                     />
                                 )}
                                 <ModuleControlButtons
-                                    moduleId={module._id}
+                                    moduleId={currentModule._id}
                                     deleteModule={(moduleId) => onRemoveModule(moduleId)}
-                                    editModule={(moduleId) => dispatch(editModule(moduleId))} />
+                                    editModule={(moduleId) => dispatch(editModule(moduleId))}
+                                />
                             </div>
-                            {module.lessons && (
+                            {currentModule.lessons && (
                                 <ListGroup className="wd-lessons rounded-0">
-                                    {module.lessons.map((lesson: any) => (
+                                    {currentModule.lessons.map((lesson: any) => (
                                         <ListGroupItem
                                             key={lesson._id}
                                             className="wd-lesson p-3 ps-1"
