3d2b90cef68e85f3463996b67c81d81aa0d1ed0d
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-11-21 18:28:11 -0500

    Update client.ts

--- app/(Kambaz)/Enrollments/client.ts
+++ app/(Kambaz)/Enrollments/client.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+
 const axiosWithCredentials = axios.create({ withCredentials: true });
+
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+
 const COURSES_API = `${HTTP_SERVER}/api/courses`;
 const USERS_API = `${HTTP_SERVER}/api/users`;
 const MODULES_API = `${HTTP_SERVER}/api/modules`;
+
 export const findAllEnrollments = async () => {
   const response = await axios.get(`${HTTP_SERVER}/api/enrollments`);
   return response.data;
@@ -11,6 +15,7 @@ export const findAllEnrollments = async () => {
 export const enrollUserInCourse = async (userId: string, courseId: string) => {
   const { data } = await axiosWithCredentials.post(
     `${USERS_API}/${userId}/courses/${courseId}`
+    // `${USERS_API}/current/enrollments/${courseId}`
   );
   return data;
 };
@@ -20,6 +25,7 @@ export const unenrollUserFromCourse = async (
 ) => {
   const { data } = await axiosWithCredentials.delete(
     `${USERS_API}/${userId}/courses/${courseId}`
+    // `${USERS_API}/current/enrollments/${courseId}`
   );
   return data;
 };
