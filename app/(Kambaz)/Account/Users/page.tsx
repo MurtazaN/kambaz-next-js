e272f7fbb82ed0a4f8ff5a9a8505b345a5025fd4
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2025-12-02 16:37:01 -0500

    Add button to create new user in Users page
    Introduced a 'Create User' button with FaPlus icon to allow adding new users. The createUser function generates a new user with default values and updates the users list. The previous Users header was commented out.

--- app/(Kambaz)/Account/Users/page.tsx
+++ app/(Kambaz)/Account/Users/page.tsx
@@ -5,11 +5,26 @@ import { useParams } from "next/navigation";
 import PeopleTable from "../../Courses/[cid]/People/Table";
 import * as client from "../client";
 import { FormControl } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa6";
 
 export default function Users() {
     const [users, setUsers] = useState<any[]>([]);
     const [role, setRole] = useState("");
     const [name, setName] = useState("");
+
+    const createUser = async () => {
+        const user = await client.createUser({
+            firstName: "New",
+            lastName: `User${users.length + 1}`,
+            username: `newuser${Date.now()}`,
+            password: "password123",
+            email: `email${users.length + 1}@neu.edu`,
+            section: "S101",
+            role: "STUDENT",
+        });
+        setUsers([...users, user]);
+    };
+
     const filterUsersByName = async (name: string) => {
         setName(name);
         if (name) {
@@ -40,7 +55,11 @@ export default function Users() {
     }, [uid]);
     return (
         <div>
-            <h3>Users</h3>
+            {/* <h3>Users</h3> */}
+            <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+                <FaPlus className="me-2" />
+                Users
+            </button>
             <FormControl onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="float-start w-25 me-2 wd-filter-by-name" />
             <select value={role} onChange={(e) => filterUsersByRole(e.target.value)}
